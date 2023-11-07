0e979ab910d1d6a8117269663a9f39b3a132fd2a
Lexikon33 <lexistda@gmail.com>
2023-11-07 12:41:14 +0100

    Oleg meine

---
+++ src/main/java/com/example/notenverwaltung/Notenverwaltung.java
@@ -0,0 +1,14 @@
+package com.example.notenverwaltung;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Notenverwaltung  {
+
+
+
+}
---
+++ src/main/java/com/example/notenverwaltung/Schulklasse.java
@@ -0,0 +1,11 @@
+package com.example.notenverwaltung;
+
+import java.util.ArrayList;
+
+public class Schulklasse {
+    String name;
+    ArrayList schüler;
+    //Notensystem
+    ArrayList unterrichtsfach;
+    //richtig?
+}
